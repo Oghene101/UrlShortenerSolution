c5f0cd7e0992062c462aa40d3b6c1d7dca101cbc
Oghene101 <karierenogheneruemu@gmail.com>
2024-11-04 12:52:01 +0100

    .

--- Url-Shortener/Models/Entities/Link.cs
+++ Url-Shortener/Models/Entities/Link.cs
@@ -9,6 +9,6 @@ public class Link(string originalUrl, string shortUrl, string shortUrlId)
     public string Id { get; set; } = Guid.NewGuid().ToString();
     [Required] public string OriginalUrl { get; set; } = originalUrl;
     [Required] public string ShortUrl { get; set; } = shortUrl;
-    [Required] public DateTime DateCreated { get; set; } = DateTime.Now;
+    [Required] public DateTime DateCreated { get; set; } = DateTime.UtcNow;
     [Required] public string ShortUrlId { get; set; } = shortUrlId;
 }
