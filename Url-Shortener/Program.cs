9728df9289d0b39d92009675aa87eb0ef7356162
Oghene101 <karierenogheneruemu@gmail.com>
2024-11-04 09:56:14 +0100

    Added condition for migration

--- Url-Shortener/Program.cs
+++ Url-Shortener/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Url_Shortener.Data;
 using Url_Shortener.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+if (app.Environment.IsProduction())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate(); // This will apply migrations if there are any pending
+    }
 }
 
 app.UseHttpsRedirection();
